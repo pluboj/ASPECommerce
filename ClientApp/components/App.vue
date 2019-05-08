121e613bf3f55a290b1abe972dead334ef7dc307
plubojacky <plubojacky@olsonresearchca.com>
2019-05-08 14:47:09 -0700

    Add product-list page

--- ClientApp/components/App.vue
+++ ClientApp/components/App.vue
@@ -1,37 +1,15 @@
 <template>
     <div>
-        <h1>Vue.js with ASP.NET Core!</h1>
-        <p>
-            The time is: {{ time }}
-        </p>
-        <p>
-            The current users of our system are:
-            <ul>
-                <li v-for="user in users" :key="user.userName">
-                    {{ user.fullName }} - {{ user.userName }}
-                </li>
-            </ul>
-        </p>
+        <product-list />
     </div>
 </template>
 
 <script>
+import ProductList from './products/List.vue';
 export default {
     name: 'app',
-    data () {
-        return {
-            time: new Date().toString(),
-            users: []
-        }
-    },
-    mounted () {
-        fetch('/api/users')
-            .then(response => {
-                return response.json()
-            })
-            .then(data => {
-                this.users = data
-            })
+    components: {
+        ProductList
     }
 }
 </script>
---
+++ ClientApp/components/products/List.vue
@@ -0,0 +1,159 @@
+<template>
+    <div class="products">
+        <h1>Products</h1>
+        <div class="list">
+            <div class="item" v-for="product in products" :key="product.slug">
+                <h3 @click="select(product)">{{ product.name }}</h3>
+                <img :src="product.thumbnail" :alt="product.name" @click="select(product)"/>
+                <p @click="select(product)">{{ product.shortDescription }}</p>
+                <p @click="select(product)">{{ product.price }}</p>
+                <hr>
+            </div>
+        </div>
+        <product-details class="details" v-if="selectedProduct" :product="selectedProduct" />
+    </div>
+</template>
+
+<script>
+import ProductDetails from './Details.vue';
+export default {
+    name: 'product-list',
+    components: {
+        ProductDetails
+    },
+    data() {
+        return {
+            products: [
+                {
+                    name: "Samsung Galaxy S8",
+                    slug: "samsung-galaxy-s8",
+                    thumbnail: "http://placehold.it/200x300",
+                    shortDescription:
+                        "Samsung Galaxy S8 Android smartphone with true edge to edge display",
+                    price: 499.99,
+                    description:
+                        "Lorem ipsum dolor sit amet consectetur adipisicing elit." + 
+                        "Perferendis tempora ad cum laudantium, omnis fugit amet iure animi corporis "+
+                        "labore repellat magnam perspiciatis explicabo maiores fuga provident a obcaecati "+
+                        "tenetur nostrum, quidem quod dignissimos, voluptatem quasi? Nisi quaerat, fugit "+
+                        "voluptas ducimus facilis impedit quod dicta, laborum sint iure nihil veniam aspernatur "+
+                        "delectus officia culpa, at cupiditate? Totam minima ut deleniti laboriosam dolores cumque in, "+
+                        "nesciunt optio! Quod recusandae voluptate facere pariatur soluta vel corrupti "+
+                        "tenetur aut maiores, cumque mollitia fugiat laudantium error odit voluptas nobis "+
+                        "laboriosam quo, rem deleniti? Iste quidem amet perferendis sed iusto tempora modi "+
+                        "illo tempore quibusdam laborum? Dicta aliquam libero, facere, maxime corporis qui "+
+                        "officiis explicabo aspernatur non consequatur mollitia iure magnam odit enim. Eligendi "+
+                        "suscipit, optio officiis repellat eos quis iure? Omnis, error aliquid quibusdam iste "+
+                        "amet nihil nisi cumque magni sequi enim illo autem nesciunt optio accusantium animi "+
+                        "commodi tenetur neque eum vitae est."
+                },
+                {
+                    name: "Samsung Galaxy S7",
+                    slug: "samsung-galaxy-s7",
+                    thumbnail: "http://placehold.it/200x300",
+                    shortDescription:
+                        "Samsung Galaxy S7 Android smartphone with true edge to edge display",
+                    price: 399.99,
+                    description:
+                        "Lorem ipsum dolor sit amet consectetur adipisicing elit." + 
+                        "Perferendis tempora ad cum laudantium, omnis fugit amet iure animi corporis "+
+                        "labore repellat magnam perspiciatis explicabo maiores fuga provident a obcaecati "+
+                        "tenetur nostrum, quidem quod dignissimos, voluptatem quasi? Nisi quaerat, fugit "+
+                        "voluptas ducimus facilis impedit quod dicta, laborum sint iure nihil veniam aspernatur "+
+                        "delectus officia culpa, at cupiditate? Totam minima ut deleniti laboriosam dolores cumque in, "+
+                        "nesciunt optio! Quod recusandae voluptate facere pariatur soluta vel corrupti "+
+                        "tenetur aut maiores, cumque mollitia fugiat laudantium error odit voluptas nobis "+
+                        "laboriosam quo, rem deleniti? Iste quidem amet perferendis sed iusto tempora modi "+
+                        "illo tempore quibusdam laborum? Dicta aliquam libero, facere, maxime corporis qui "+
+                        "officiis explicabo aspernatur non consequatur mollitia iure magnam odit enim. Eligendi "+
+                        "suscipit, optio officiis repellat eos quis iure? Omnis, error aliquid quibusdam iste "+
+                        "amet nihil nisi cumque magni sequi enim illo autem nesciunt optio accusantium animi "+
+                        "commodi tenetur neque eum vitae est."
+                },
+                {
+                    name: "Samsung Galaxy S6",
+                    slug: "samsung-galaxy-s6",
+                    thumbnail: "http://placehold.it/200x300",
+                    shortDescription:
+                        "Samsung Galaxy S6 Android smartphone with true edge to edge display",
+                    price: 299.99,
+                    description:
+                        "Lorem ipsum dolor sit amet consectetur adipisicing elit." + 
+                        "Perferendis tempora ad cum laudantium, omnis fugit amet iure animi corporis "+
+                        "labore repellat magnam perspiciatis explicabo maiores fuga provident a obcaecati "+
+                        "tenetur nostrum, quidem quod dignissimos, voluptatem quasi? Nisi quaerat, fugit "+
+                        "voluptas ducimus facilis impedit quod dicta, laborum sint iure nihil veniam aspernatur "+
+                        "delectus officia culpa, at cupiditate? Totam minima ut deleniti laboriosam dolores cumque in, "+
+                        "nesciunt optio! Quod recusandae voluptate facere pariatur soluta vel corrupti "+
+                        "tenetur aut maiores, cumque mollitia fugiat laudantium error odit voluptas nobis "+
+                        "laboriosam quo, rem deleniti? Iste quidem amet perferendis sed iusto tempora modi "+
+                        "illo tempore quibusdam laborum? Dicta aliquam libero, facere, maxime corporis qui "+
+                        "officiis explicabo aspernatur non consequatur mollitia iure magnam odit enim. Eligendi "+
+                        "suscipit, optio officiis repellat eos quis iure? Omnis, error aliquid quibusdam iste "+
+                        "amet nihil nisi cumque magni sequi enim illo autem nesciunt optio accusantium animi "+
+                        "commodi tenetur neque eum vitae est."
+                },
+                {
+                    name: "Samsung Galaxy S5",
+                    slug: "samsung-galaxy-s5",
+                    thumbnail: "http://placehold.it/200x300",
+                    shortDescription:
+                        "Samsung Galaxy S5 Android smartphone with display",
+                    price: 199.99,
+                    description:
+                        "Lorem ipsum dolor sit amet consectetur adipisicing elit." + 
+                        "Perferendis tempora ad cum laudantium, omnis fugit amet iure animi corporis "+
+                        "labore repellat magnam perspiciatis explicabo maiores fuga provident a obcaecati "+
+                        "tenetur nostrum, quidem quod dignissimos, voluptatem quasi? Nisi quaerat, fugit "+
+                        "voluptas ducimus facilis impedit quod dicta, laborum sint iure nihil veniam aspernatur "+
+                        "delectus officia culpa, at cupiditate? Totam minima ut deleniti laboriosam dolores cumque in, "+
+                        "nesciunt optio! Quod recusandae voluptate facere pariatur soluta vel corrupti "+
+                        "tenetur aut maiores, cumque mollitia fugiat laudantium error odit voluptas nobis "+
+                        "laboriosam quo, rem deleniti? Iste quidem amet perferendis sed iusto tempora modi "+
+                        "illo tempore quibusdam laborum? Dicta aliquam libero, facere, maxime corporis qui "+
+                        "officiis explicabo aspernatur non consequatur mollitia iure magnam odit enim. Eligendi "+
+                        "suscipit, optio officiis repellat eos quis iure? Omnis, error aliquid quibusdam iste "+
+                        "amet nihil nisi cumque magni sequi enim illo autem nesciunt optio accusantium animi "+
+                        "commodi tenetur neque eum vitae est."
+                }
+            ],
+            selectedProduct: null
+        }
+    },
+    methods: {
+        select(product) {
+            this.selectedProduct = product;
+        }
+    }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box !important;
+}
+.products {
+  padding: 20px;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.list,
+.details {
+  width: 50%;
+  float: left;
+}
+.list .item {
+  width: 50%;
+  float: left;
+  padding: 20px 10px 20px 0;
+}
+.list img,
+.list h3,
+.list p {
+  cursor: pointer;
+}
+.list img {
+  width: 100px;
+}
+</style>
+
